8395a061958c9130cd2fae1b9311bababb91f829
Christina Yujeong Yun <yjc.yun@gmail.com>
2021-05-15 22:12:21 -0400

    jbook: handle error

--- 4-jbook/src/components/code-cell.tsx
+++ 4-jbook/src/components/code-cell.tsx
@@ -10,12 +10,14 @@ import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [error, setError] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setError(output.error);
     }, 1000);
 
     return () => {
@@ -32,7 +34,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} error={error} />
       </div>
     </Resizable>
   );
--- 4-jbook/src/components/preview.tsx
+++ 4-jbook/src/components/preview.tsx
@@ -3,6 +3,7 @@ import './preview.css';
 
 interface PreviewProps {
   code: string;
+  error: string;
 }
 
 const html = `
@@ -13,13 +14,22 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
+          const handleError = err => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          };
+
+          window.addEventListener('error', event => {
+            event.preventDefault();
+            handleError(event.error);
+          });
+
           window.addEventListener('message', event => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+              handleError(err);
             }
           }, false);
         </script>
@@ -27,7 +37,7 @@ const html = `
     </html>
   `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, error }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
@@ -46,6 +56,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         srcDoc={html}
         sandbox='allow-scripts'
       />
+      {error && <div className='preview-error'>{error}</div>}
     </div>
   );
 };
