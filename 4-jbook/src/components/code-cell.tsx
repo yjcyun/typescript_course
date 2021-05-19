e3be87aac5cfdac24fdbf10f5e440b9fed1ab68a
Christina Yujeong Yun <yjc.yun@gmail.com>
2021-05-18 21:03:33 -0400

    jbook: implement cumulative code execution

--- 4-jbook/src/components/code-cell.tsx
+++ 4-jbook/src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -5,6 +6,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hook/use-actions';
 import { useTypedSelector } from '../hook/use-typed-selector';
+import { useCumulativeCode } from '../hook/use-cumulative-code';
 
 /**
  * Debouncing: update state -> 1second passes without any updates to 'input' state then run the bundling logic
@@ -18,15 +20,23 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
+  const cumulativeCode = useCumulativeCode(cell.id);
+
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cumulativeCode);
+      return;
+    }
+
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [createBundle, cell.content, cell.id]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cumulativeCode, createBundle, cell.content, cell.id]);
 
   return (
     <Resizable direction='vertical'>
@@ -37,7 +47,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} error={bundle.error} />}
+        <div className='progress-wrapper'>
+          {!bundle || bundle.loading ? (
+            <div className='progress-cover'>
+              <progress className='progress is-small is-primary' max='100'>
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} error={bundle.error} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
